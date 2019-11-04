4a93612344f86b798b7e2da37b4b15204bc85603
Ignacio <jose.schmid@ccc.ufcg.edu.br>
2019-11-04 20:58:38 -0300

    creating base

--- broker/api.go
+++ broker/api.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func pub(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(w, "hey, this is pub")
+	
+}
+
+func sub(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hey, this is sub")
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/pub", pub)
+	router.HandleFunc("/sub", sub)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
