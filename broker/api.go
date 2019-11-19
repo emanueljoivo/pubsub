ff08bfda3c5d68a76f7caea3fe2d0e8f3a18626f
Emanuel Joívo <emanueljoivo@gmail.com>
2019-11-19 20:57:45 -0300

    Created func to dispatch messages from the broker to the sentinel

--- broker/api.go
+++ broker/api.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gorilla/mux"
 )
 
 const SentinelAddress string = "localhost:5002"
+
+var (
+	RequestError = errors.New("request could not be completed")
+)
 
 type TopicMessage struct {
 	Topic string
@@ -22,6 +31,32 @@ type TopicRequest struct {
 	Topic string
 	Offset int
 }
+
+func dispatchMsg(topic TopicMessage) {
+	var buffer bytes.Buffer
+	enc := gob.NewEncoder(&buffer)
+	_ = enc.Encode(topic)
+	var jsonStr = buffer.Bytes()
+
+	requestUrl := SentinelAddress + "/newStorage/" + strconv.Itoa(10)
+
+	req, _ := http.NewRequest("POST", requestUrl, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Print(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("response Headers:", resp.Header)
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println("response Body:", string(body))
+}
+
 
 func pub(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("hey, this is pub")
@@ -36,12 +71,12 @@ func pub(w http.ResponseWriter, r *http.Request) {
 
 	messageJson, err := json.Marshal(message)
 	if err != nil{
-		panic(err)
+		log.Println(RequestError)
 	}
 
 	w.Header().Set("Content-Type","application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write(messageJson)
+	_, _ = w.Write(messageJson)
 	
 }
 
@@ -51,17 +86,17 @@ func sub(w http.ResponseWriter, r *http.Request) {
 
 	err := json.NewDecoder(r.Body).Decode(&request)
 	if err != nil {
-		panic(err)
+		log.Println(err)
 	}
 
 	requestJson, err := json.Marshal(request)
 	if err != nil{
-		panic(err)
+		log.Println(RequestError)
 	}
 
 	w.Header().Set("Content-Type","application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write(requestJson)
+	_, _ = w.Write(requestJson)
 }
 
 func main() {
